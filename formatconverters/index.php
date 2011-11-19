7202792131431ef8173de7bc55dcf13dfc61a582
Dominik Zogg <dominik.zogg@gmail.com>
2011-11-19 01:06:07 +0100

    taf2xml example

---
+++ formatconverters/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require('taf2array/taf2array.php');
+require('xml2array/xml2array.php');
+
+$strToParse = '{Blg Date=18.01.03 MType=0 Orig=1 {Bk AccId=11050 CAcc=div Flags=1 OpId=81222 PkKey=2002015002 Text="Muster Buchungstext" Type=0 ValNt=1100 }{Bk AccId=23010 CAcc=11050 MkTxB=1 TaxId=frei Text="1. Rate" Type=1 ValBt=1100 }{Bk AccId=84.500 BType=1 CAcc=23010 Text="RNR: 81222 Art: Rate" Type=1 ValNt=1100 }}';
+
+$taf = new taf2array();
+$taf->set($strToParse);
+$arrToTest = $taf->getArray();
+printData($arrToTest, false);
+
+$xml = new xml2array();
+$xml->set($arrToTest);
+
+echo htmlspecialchars($xml->getString());
+
+
+function printData($arrData, $boolDie = true)
+{
+    echo '<pre>'; print_r($arrData); echo '</pre>';
+    if($boolDie)
+    {
+        die();
+    }
+}
