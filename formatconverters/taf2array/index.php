fbd7139301e42c349b64fdd7a0033490e3b3559b
Dominik Zogg <dominik.zogg@gmail.com>
2011-11-19 01:04:51 +0100

    readd taf2array on a diffrent place

---
+++ formatconverters/taf2array/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require('taf2array.php');
+
+$strToParse = '{Blg Date=18.01.03 MType=0 Orig=1 {Bk AccId=11050 CAcc=div Flags=1 OpId=81222 PkKey=2002015002 Text="Muster Buchungstext" Type=0 ValNt=1100 }{Bk AccId=23010 CAcc=11050 MkTxB=1 TaxId=frei Text="1. Rate" Type=1 ValBt=1100 }{Bk AccId=84.500 BType=1 CAcc=23010 Text="RNR: 81222 Art: Rate" Type=1 ValNt=1100 }}';
+
+$taf = new taf2array();
+
+$taf->set($strToParse);
+echo $taf->getString();
+$arrToTest = $taf->getArray();
+printData($arrToTest, false);
+$taf->set($arrToTest);
+echo $taf->getString();
+
+function printData($arrData, $boolDie = true)
+{
+    echo '<pre>'; print_r($arrData); echo '</pre>';
+    if($boolDie)
+    {
+        die();
+    }
+}
