488f9a3cfb5fb75a2e2eac0eb64ca034e6fa7d21
Dominik Zogg <dominik.zogg@gmail.com>
2011-12-01 05:39:31 +0100

    add printdata function

--- httpclient/index.php
+++ httpclient/index.php
@@ -12,3 +12,12 @@ catch(Exception $e)
 {
     echo $e->getMessage();
 }
+
+function printData($arrData, $boolDie = true)
+{
+    echo '<pre>'; print_r($arrData); echo '</pre>';
+    if($boolDie)
+    {
+        die();
+    }
+}
